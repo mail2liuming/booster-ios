d048b7029a43a151740bdeff0800035148306640
mail2liuming <mail.liuming@qq.com>
2017-01-13 15:34:35 +1300

    Initial Commit

---
+++ boostertesttask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  boostertesttask
+//
+//  Created by liu on 17/1/13.
+//  Copyright © 2017年 liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
